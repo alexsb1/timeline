93069f718e85e5a51968c0acd518099e36addf1d
alexsb1 <39804539+alexsb1@users.noreply.github.com>
2020-01-08 17:29:35 +0000

    First commit
    First commit. Includes first version of directory layouts, default shiny (R) app and datasets.

---
+++ shiny/app.R
@@ -0,0 +1,70 @@
+# Timeline of concurrent events and climates
+#
+# By Alex Searle-Barnes
+#
+# name@domain.com
+#
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#    http://shiny.rstudio.com/
+#
+
+# ----- set libraries ----
+library(shiny)
+library(rsconnect)
+library(tidyverse)
+
+#end of libraries
+
+#---- data import ----
+#start of data import
+
+
+
+# end of data import
+
+# ---- timeline UI ----
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
