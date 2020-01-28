67491b7fbe29f67fbeae9d12df47b952c43bbd0b
alexsb1 <39804539+alexsb1@users.noreply.github.com>
2020-01-28 16:51:04 +0000

    Start of temperature anomaly test file. Imported data and quick ggplot to visualise data.

---
+++ scripts/R/tempAnomalyTest.R
@@ -0,0 +1,31 @@
+# Alex Searle-Barnes
+#
+# https://github.com/alexsb1/timeline
+#
+# Script to test timelines with the temp_anomaly_800000_ybp.csv file.
+# Temperature anomaly from the past 800,000 years.
+# Look at Zachos curve. Find data to import into R.
+#
+
+# ----- set libraries ----library(ggplot2)
+library(tidyverse)
+#end of libraries
+
+#---- set variables ----
+
+
+# end of variables
+
+#---- data import ----
+tempAnom <- read.csv("data/raw/temp_anomaly_800000_ybp.csv")
+
+#----End of data import
+
+#---- Clean code
+# Note that the column years before present has negative numbers (this is a tautology).
+
+
+#---- temperature code
+
+ggplot(data = tempAnom)+
+  geom_line(aes(x = ))
